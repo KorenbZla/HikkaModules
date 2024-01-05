044e1e4d4f3ea62f5202db398882f3c428e98d5a
KorenbZla <136430440+KorenbZla@users.noreply.github.com>
2024-01-05 13:04:45 +0300

    Add files via upload

---
+++ Melody.py
@@ -0,0 +1,44 @@
+# Name: Melody
+# Author: Felix?
+# Commands:
+# .mmoney
+# scope: hikka_only
+# meta developer: @KorenbZla
+
+
+__version__ = (1, 0, 0)
+
+
+from .. import loader, utils
+import asyncio
+
+@loader.tds
+class MelodyMod(loader.Module):
+    """Поет песни Instasamka"""
+    strings = {"name": "Melody"}
+
+    async def mmoneycmd(self, message):
+        """Песня «За деньги да»"""
+        lyrics = [
+              "Я вообще делаю что хочу",
+              "Хочу импланты — звоню врачу",
+              "Кто меня не любит — я вас не слышу",
+              "Вы просто мне завидуете, я молчу",
+              "Я не продаюсь, но за деньги — да",
+              "Мой продюссер говорит я поп-звезда",
+              "И кстати, мой продюссер — это мой муж, да",
+              "Я не скажу в ответ ничего на вид",
+              "И не скажу «привет» если бабок нет",
+              "Слышу любимый звук — это звон монет",
+              "Они тянут все мне руку, это мой концерт",
+              "Не завожу подруг, но за деньги — да",
+              "Я подумаю потом, но скажу сразу «да»",
+              "За деньги, да, за деньги, да",
+              "За деньги, да",
+              "Деньги— Деньги— Деньги— Деньги— Да— Да— Да— Да—",
+              ]
+        for line in lyrics:
+          #  message = await utils.answer(message, line)
+          #
+            await asyncio.sleep(3)
+            await utils.answer(message, line)
