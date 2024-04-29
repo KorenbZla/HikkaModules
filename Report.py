ac48b67ff7cfecf189f11e29fd886d08ca021313
Serezha dend1y <123255255+dend1yz@users.noreply.github.com>
2024-04-29 16:24:30 +0300

    reworked.
    added config
    added in client_info create chat 
    added functions .chaton .chat 
    accept reports from any chat now.

--- Report.py
+++ Report.py
@@ -1,80 +1,160 @@
-# Name: Report
-# Author: dend1yya
-# Commands:
-# .report | .reporton | .reportoff | .reporthelp
-# scope: hikka_only
-# meta developer: @AuroraModules
-from telethon import functions
+# *      _                             __  __           _       _
+# *     / \  _   _ _ __ ___  _ __ __ _|  \/  | ___   __| |_   _| | ___  ___ 
+# *    / _ \| | | | '__/ _ \| '__/ _` | |\/| |/ _ \ / _` | | | | |/ _ \/ __|
+# *   / ___ \ |_| | | | (_) | | | (_| | |  | | (_) | (_| | |_| | |  __/\__ \
+# *  /_/   \_\__,_|_|  \___/|_|  \__,_|_|  |_|\___/ \__,_|\__,_|_|\___||___/
+# *
+# *                          © Copyright 2024
+# *
+# *                      https://t.me/AuroraModules
+# *
+# * 🔒 Code is licensed under CC-BY-NC-ND 4.0 unless otherwise specified.
+# * 🌐 https://creativecommons.org/licenses/by-nc-nd/4.0/
+# * ⛔️ You CANNOT edit this file without direct permission from the author.
+# * ⛔️ You can redistribute this file without any changes.
+#meta developer: @AuroraModules
+#Author: @dend1yya
 
+
+from hikkatl.types import Message  # type: ignore
+from telethon.tl.functions.channels import InviteToChannelRequest  # type: ignore
+from telethon.tl.functions.users import GetFullUserRequest #type: ingore
+from telethon.tl.functions.channels import CreateChannelRequest, InviteToChannelRequest #type: ignore
+from telethon.tl.types import ChatAdminRights #type: ignore
 from .. import loader, utils
+import logging
+
+logger = logging.getLogger(__name__)
 
 @loader.tds
 class ReportsMod(loader.Module):
-    """Модуль для отправки репортов"""
+    """Модуль для отправки репортов."""
     strings = {
         "name": "Reports",
-        "report_enabled": "<b><emoji document_id=5345863488872916640>✅</emoji> Репорты включены.\nСоздан чат 'reports'.</b>",
-        "report_disabled": "<b><emoji document_id=5467928559664242360>❗️</emoji> Репорты выключены. Чат 'reports' удален.</b>",
-        "report_sent": "<b><emoji document_id=5447644880824181073>⚠️</emoji> Новый репорт:</b>\n\n<b><emoji document_id=5219822691409733126>⚙️</emoji> Кто отправил:</b> {}\n<b><emoji document_id=5467666648263564704>❓</emoji> На кого отправлен:</b> {}\n<b><emoji document_id=5972183258090179945>💬</emoji> Причина:</b> {}",
-        "not_enabled": "<b><emoji document_id=5467928559664242360>❗️</emoji> Репорты не включены. Включите командой .reporton.</b>",
-        "already_enabled": "<b><emoji document_id=5231200819986047254>📊</emoji> Репорты уже включены.</b>",
-        "already_disabled": "<b><emoji document_id=5231200819986047254>📊</emoji> Репорты уже выключены.</b>",
-        "module_help": "<b><emoji document_id=5226512880362332956>📖</emoji> Краткая документация по использованию модуля:\n\n<b><emoji document_id=5447644880824181073>⚠️</emoji> Важное примечание! Модуль reports работает только для одного чата (пока что) И если вы попытаетесь использовать модуль в разных чатах, то у вас ничего не получится\n\n<emoji document_id=5382187118216879236>❓</emoji>Как разрешить команду всем пользователям? К сожалениию это можно сделать только через tsec\n\n<emoji document_id=5282843764451195532>🖥</emoji> Чтобы выдать tsec пользователю используй эту команду: tsec <user/chat/sgroup> [цель пользователя или чата] [правило (команда/модуль)] [время] - Добавить новое правило таргетированной безопасности\n\n<emoji document_id=5310044041444865824>💻</emoji>Или вы можете выдать пользователю группу Owner через команду: !owneradd (username)/(reply)</b>",
-        "invalid_args": "<b><emoji document_id=5467666648263564704>❓</emoji> Укажите пользователя и причину репорта.</b>",
-        "wrong_format": "<b><emoji document_id=5980953710157632545>❌</emoji> Неверный формат.\n<emoji document_id=5467928559664242360>❗️</emoji>Используйте: report (юзернейм) (причина)</b>",
-        "report_sented": "<b><emoji document_id=5980930633298350051>✅</emoji> Ваш репорт успешно отправлен!</b>\n\n<i><emoji document_id=5255835635704408236>👤</emoji> Администрация свяжется с вами в ближайшее время, и вынесет вердикт.</i>"
+        "admin_id": "ADMIN_ID",
+        "loading": "<b>Loading...</b>",
+        "group_created": "<b>Report Channel accessed and admin(s) invited if specified.</b>",
+        "group_error": "<b>Error accessing the report channel.</b>",
+        "no_admins": "<b>No admins configered to ivnite.</b>",
+        "reports_enabled": "Report reception enabled in this chat.",
+        "reports_disabled": "Report reception disabled in this chat.",
+        "reports_not_enabled": "<b>Reports not enabled in this chat.</b>",
+        "not_args": "<b>Usage: .report {userID/@username} (reason)</b>",
+        "parts_both_2": "<b>Please provide both a user and a reason.</b>",
+        "success": "<b>Report submitted successfully.</b>",
+        "fail": "<b>Failed to submit the report.</b>",
     }
-    strings_en = {
-        "report_enabled": "<b>Report on.\n<emoji document_id=5895457880710058528>💬</emoji>Created chat 'report'.</b>",
-        "report_disabled": "<b>Report off. Chat 'reports' deleted.</b>",
-        "report_sent": "<b>New report:</b>\n\n<b>Who send:</b>{}\n<b>Sent to:</b>{}\n<b>Reason:</b> {}",
-        "not_enabled": "<b>Reports not included. Turn them on comand reporton.</b>",
-        "already_enabled": "<b>Reports are already inculuded</b>",
-        "already_disabled": "<b>Reports already disabled.</b>",
-        "module_help": "<b>Brief documentation on using the module:\n\nImportant note! The module only works for one chat (for now) And if you try to use the module in different chats, it will not work\n\nHow to allow a command to all users?\n\nUnfortunately this can only be done through tsec\n\nTo issue tsec use this command: tsec <user/chat/sgroup> [target user or chat] [rule (command/module)] [time] - Add new targeted security rule\n\nOr u cane use 'owneradd' example: owneradd(user)/(reply)</b>",
-        "invalid_args": "<b>Specify the user and reason for the report.</b>",
-        "wrong_format": "<b>Wrong format. Use: report (username) (reason)</b>",
-        "report_sented": "<b>Report succefully sented.</b>"
+
+    strings_ru = {
+        "admin_id": "ADMIN_ID",
+        "loading": "<b>Загрузка...</b>",
+        "group_created": "<b>Группа для репортов создана, и администраторы были приглашены.</b>",
+        "group_error": "<b>Ошибка при создании канала с репортами.",
+        "no_admins": "<b>Нету администраторов которых можно добавить в группу.</b>",
+        "reports_enabled": "<b>Репорты включены в этом чате</b>",
+        "reports_disabled": "<b>Репорты выключены в этом чате</b>",
+        "reports_not_enabled": "<b>Репорты не включены в этом чате.</b>",
+        "not_args": "<b>Использование: .report{userID/@username} (причина)</b>",
+        "parts_both_2": "<b>Пожалуйста введите имя пользователя и причину.</b>",
+        "success": "<b>Репорт успешно отправлен.</b>",
+        "fail": "<b>Ошибка при отправке репорта.</b>"
     }
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            "admin_id",
+            None,
+            lambda: self.strings["admin_id"]
+        )
 
     async def client_ready(self, client, db):
         self.client = client
-        self.reports_chat = None
+        self.db = db
+        logger.info("AuroraReports installed!")
+        try:
+            result = await client(CreateChannelRequest(
+                title="Aurora-Reports",
+                about="Группа, куда приходят репорты",
+                megagroup=True
+            ))
+            self.Aurora_Reports = result.chats[0]
+        except Exception as e:
+            logger.error(f"Failed to create report group: {str(e)}")
 
-    async def reportoncmd(self, message):
-        """Включить репорты"""
-        if not self.reports_chat:
-            result = await self.client(functions.messages.CreateChatRequest(users=['reports'], title='Reports'))
-            self.reports_chat = result.chats[0].id
-            await utils.answer(message, self.strings["report_enabled"])
-        else:
-            await utils.answer(message, self.strings["already_enabled"])
+    async def addadminscmd(self, message):
+        """Приглашает администраторов в супергруппу"""
+        await utils.answer(message, self.strings["loading"])
+        try:
+            admin_ids = self.config.get("admin_id")
+            if not admin_ids:
+                await utils.answer(message, self.strings["no_admins"])
+                return
+            
+            if isinstance(admin_ids, list):
+                for admin_id in admin_ids:
+                    await self.client(InviteToChannelRequest(
+                        channel=self.Aurora_Reports,
+                        users=[admin_id]
+                    ))
+            else:
+                await self.client(InviteToChannelRequest(
+                    channel=self.Aurora_Reports,
+                    users=[admin_ids]
+                ))
+            await utils.answer(message, self.strings["group_created"])
+        except Exception as e:
+            logger.error(f"Failed to invite admins to the report group: {str(e)}")
+            await utils.answer(message, self.strings["group_error"])
 
-    async def reportoffcmd(self, message):
-        """Отключить репорты"""
-        if self.reports_chat:
-            await self.client(functions.messages.DeleteChatRequest(chat_id=self.reports_chat))
-            self.reports_chat = None
-            await utils.answer(message, self.strings["report_disabled"])
-        else:
-            await utils.answer(message, self.strings["already_disabled"])
+    async def chatoncmd(self, message):
+        """Включает прием репортов в этом чате."""
+        chat_id = str(message.chat_id)
+        self.db.set("ReportsMod", f"report_active_{chat_id}", True)
+        await utils.answer(message, self.strings["reports_enabled"])
+
+    async def chatoffcmd(self, message):
+        """Выключает прием репортов в этом чате."""
+        chat_id = str(message.chat_id)
+        self.db.set("ReportsMod", f"report_active_{chat_id}", False)
+        await utils.answer(message, self.strings["reports_disabled"])
+
 
     async def reportcmd(self, message):
-        """Отправить репорт"""
-        if self.reports_chat:
-            args = utils.get_args_raw(message)
-            if not args:
-                return await utils.answer(message, self.strings["invalid_args"])
-            try:
-                target, reason = args.split(' ', 1)
-            except ValueError:
-                return await utils.answer(message, self.strings["wrong_format"])
-            report_text = self.strings["report_sent"].format(message.sender_id, target, reason)
-            await self.client.send_message(self.reports_chat, report_text)
-            await utils.answer(message, self.strings["report_sented"])
-        else:
-            await utils.answer(message, self.strings["not_enabled"])
+        """Отправляет репорт администрации."""
+        chat_id = str(message.chat_id)
+        report_active = self.db.get("ReportsMod", f"report_active_{chat_id}", False)
 
-    async def reporthelpcmd(self, message):
-        """Показать помощь по модулю."""
-        await utils.answer(message, self.strings["module_help"])
+        if not report_active:
+            await utils.answer(message, self.strings["reports_not_enabled"])
+            return
+
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, self.strings["not_args"])
+            return
+
+        parts = args.split(maxsplit=1)
+        if len(parts) < 2:
+            await utils.answer(message, self.strings["parts_both_2"])
+            return
+
+        user_id_or_name, reason = parts
+        try:
+            if user_id_or_name.isdigit():
+                user_id = int(user_id_or_name)
+            else:
+                user_id = user_id_or_name
+
+            user = await self.client.get_entity(user_id)
+            reporter = await self.client.get_entity(message.sender_id)
+
+            report_message = (f"<b>🚨 Report Submitted 🚨</b>\n\n"
+                              f"<b>Reporter:</b> @{reporter.username if reporter.username else 'unknown'} (ID: {reporter.id})\n"
+                              f"<b>Reported User:</b> @{user.username if user.username else 'unknown'} (ID: {user.id})\n"
+                              f"<b>Reason:</b> {reason}")
+
+            await self.client.send_message(self.Aurora_Reports, report_message)
+            await utils.answer(message, self.strings["success"])
+        except Exception as e:
+            logger.error(f"Failed to process the report: {str(e)}")
+            await utils.answer(message, self.strings["fail"])
